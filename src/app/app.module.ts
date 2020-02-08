78ddc0e3c8d11689ecb8e1919fc952421d67c70d
Anders <anders_thy@rocketmail.com>
2020-02-08 22:30:26 +0100

    init nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ButtonComponent } from './button/button.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, ButtonComponent],
+  declarations: [AppComponent, ListComponent, ButtonComponent, NavComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]

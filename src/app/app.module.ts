57063b05ec6a1b8a21a597863bff6d2863c202d0
Anders <anders_thy@rocketmail.com>
2020-02-08 20:49:23 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ButtonComponent } from './button/button.component';
-import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    ButtonComponent,
-    InputComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListComponent, ButtonComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  clickMessage: any;
+  newItem = '';
+  msg = '';
   list: Array<any> = [
     {
       id: 1,
@@ -17,10 +18,19 @@ export class ListComponent implements OnInit {
       name: 'Hero 2'
     }
   ];
+
   onClickMe(e) {
     const target = this.list.find(x => x.id === e);
     const deleteMe = this.list.indexOf(target);
     this.list.splice(deleteMe, 1);
+  }
+
+  onAddList() {
+    if (this.newItem.length > 0) {
+      this.list.push({ name: this.newItem, id: this.list.length + 1 });
+      return;
+    }
+    alert('You need to enter a new item');
   }
 
   constructor() {}
